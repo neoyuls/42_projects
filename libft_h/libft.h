1b1f5b298b64a5c6f880bcdef1164162883910a3
neoyuls <vernon.juli@gmail.com>
2026-07-15 16:53:16 +0200

    started building libft.h file

---
+++ libft_h/libft.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: neoyuls <neoyuls@student.42.es>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/15 16:45:27 by neoyuls           #+#    #+#             */
+/*   Updated: 2026/07/15 16:50:41 by neoyuls          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 	funciones obligatorias a escribir:
+* isalpha
+* isdigit
+* isalnum
+* isascii
+* isprint
+* strlen
+* memset
+* bzero
+* memcpy
+* memmove
+* strlcpy
+* strlcat
+* toupper
+* tolower
+* strchr
+* strrchr
+* strncmp
+* memchr
+* memcmp
+* strnstr
+* atoi
+* 	Con malloc:
+* calloc
+* strdup
+*
+*	funciones adicionales:
+* ft_substr
+* fr_strjoin
+* ft_strtrim
+*
+*/
