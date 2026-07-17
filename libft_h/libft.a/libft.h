018af2fe5d14002c27cd135de6661876da955f8a
neoyuls <vernon.juli@gmail.com>
2026-07-18 00:30:15 +0200

    first 3 functions written

---
+++ libft_h/libft.a/libft.h
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: neoyuls <neoyuls@student.42.es>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/15 16:45:27 by neoyuls           #+#    #+#             */
+/*   Updated: 2026/07/18 00:29:47 by neoyuls          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* PARTE 1: 	funciones obligatorias a escribir:
+* isalpha
+* isdigit
+* isalnum
+* isascii
+* isprint
+* strlen
+* memset
+* bzero
+* memcpy
+* memmove
+* strlcpy
+* strlcat
+* toupper
+* tolower
+* strchr
+* strrchr
+* strncmp
+* memchr
+* memcmp
+* strnstr
+* atoi
+* 	Con malloc:
+* calloc
+* strdup
+*/
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+/*
+*
+*	funciones adicionales:
+* ft_substr
+* fr_strjoin
+* ft_strtrim
+*
+*/
