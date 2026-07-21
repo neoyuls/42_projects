2da168f160cd36f9461120eb58140e1ea89d8909
neoyuls <vernon.juli@gmail.com>
2026-07-21 17:19:38 -0600

    finished ft_atoi.c, for real, read up on overflow behaviour, which 42 spec does not require

--- libft_h/libft.a/src/ft_atoi.c
+++ libft_h/libft.a/src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/21 14:36:33 by neoyuls           #+#    #+#             */
-/*   Updated: 2026/07/21 14:36:35 by neoyuls          ###   ########.fr       */
+/*   Updated: 2026/07/21 17:18:48 by neoyuls          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,9 @@ int	ft_atoi(const char *nbr)
 	i = 0;
 	num = 0;
 	sign = 1;
+	while ((nbr[i] == ' ')
+		|| (nbr[i] >= '\t' && nbr[i] <= '\r'))
+		i++;
 	if (nbr[i] == '-')
 	{
 		sign = -1;
@@ -36,24 +39,26 @@ int	ft_atoi(const char *nbr)
 	}
 	else if (nbr[i] == '+')
 		i++;
-	while (nbr[i] >= 48 && nbr[i] <= 57)
+	while (nbr[i] >= '0' && nbr[i] <= '9')
 	{
-		num = (num * 10) + (nbr[i] - 48);
+		num = (num * 10) + (nbr[i] - '0');
 		i++;
 	}
 	return (num * sign);
 }
-
 /*
 #include <stdio.h>
 #include <stdlib.h>
 
-int	main(int ac, char **av)
+//int	main(int ac, char **av)
+int	main(void)
 {
-	if (ac != 2)
-		return (1);
-	int result = ft_atoi(av[1]);
-	int r2 = atoi(av[1]);
+//	if (ac != 2)
+//	return (1);
+//	int result = ft_atoi(av[1]);
+//	int r2 = atoi(av[1]);
+	int result = ft_atoi("\t\n\r-300");
+	int r2 = atoi("\t\n\r-300");
 	printf("\tft_atoi output \t= %d \n", result);
 	printf("\tstdlib atoi \t= %d \n", r2);
 	return 0;
