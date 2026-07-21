f83719f55216d1ad96b235b63335734d7071c0d2
neoyuls <vernon.juli@gmail.com>
2026-07-21 17:47:53 -0600

    worked on strlcpy

--- libft_h/libft.a/src/ft_strlcpy.c
+++ libft_h/libft.a/src/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 18:10:17 by neoyuls           #+#    #+#             */
-/*   Updated: 2026/07/21 14:16:25 by neoyuls          ###   ########.fr       */
+/*   Updated: 2026/07/21 17:47:28 by neoyuls          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 // I DONT LIKE THIS FUNCTION
 unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	if (size == 0)
-		return (0);
+		return (size);
+	while (i < size && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size >= i)
+		dest[i] = '\0';
+	return (i);
 }
