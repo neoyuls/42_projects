be3313394417a01ca741c65ecdcad4f7d5cb2058
neoyuls <vernon.juli@gmail.com>
2026-07-23 11:10:27 -0600

    finished(?) strlcat

--- libft_h/libft.a/src/ft_strlcat.c
+++ libft_h/libft.a/src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/23 10:53:04 by neoyuls           #+#    #+#             */
-/*   Updated: 2026/07/23 10:53:06 by neoyuls          ###   ########.fr       */
+/*   Updated: 2026/07/23 11:10:17 by neoyuls          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 	if (size == 0)
 		return (i);	
 	if (j > size)
-		return (i + j);
+		return (i + size);
 	i = 0;
-	while (j < size - 1 && src[i])
+	while ((i + j) < size - 1 && src[i])
 	{
 		dest[j + i] = src[i];
 		i++;
@@ -36,7 +36,6 @@ unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 }
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -51,18 +50,11 @@ int	main(int ac, char **av)
 	}
 	unsigned int size = (unsigned int)atoi(av[3]);
 	char *dest = av[1];
-	char *dest2 = av[1];
 	char *src = av[2];
-	char *src2 = av[2];
 	printf("my function: \n");
 	unsigned int r1 = ft_strlcat(dest, src, size);
 	printf("\tsource: %s\n", src);
 	printf("\tdest: %s\n", dest);
 	printf("\treturn: %d\n", r1);
-	unsigned int r2 = strlcat(dest2, src2, size);
-	printf("string.h function: \n");
-	printf("\tsource: %s\n", src2);
-	printf("\tdest: %s\n", dest2);
-	printf("\treturn: %d\n", r2);
 	return (0);
 }
--- libft_h/libft.a/src/ft_strlcpy.c
+++ libft_h/libft.a/src/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 18:10:17 by neoyuls           #+#    #+#             */
-/*   Updated: 2026/07/23 10:22:48 by neoyuls          ###   ########.fr       */
+/*   Updated: 2026/07/23 10:26:36 by neoyuls          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ unsigned int	ft_strlcpy(char *dest, const char *src, unsigned int size)
 	dest[j] = '\0';
 	return (i);
 }
-
+/*
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -61,3 +61,4 @@ int	main(int ac, char **av)
 	printf("\treturn: %d\n", r2);
 	return (0);
 }
+*/
