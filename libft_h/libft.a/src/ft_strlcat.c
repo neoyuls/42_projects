e1554d84f93531fab556390fe575349da87eff8d
neoyuls <vernon.juli@gmail.com>
2026-07-23 10:53:18 -0600

    added 42 header to strlcat

--- libft_h/libft.a/src/ft_strlcat.c
+++ libft_h/libft.a/src/ft_strlcat.c
@@ -1,8 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/23 10:53:04 by neoyuls           #+#    #+#             */
+/*   Updated: 2026/07/23 10:53:06 by neoyuls          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
 	unsigned int	i;
 	unsigned int	j;
 
+	i = 0;
+	j = 0;
 	while (src[i])
 		i++;
 	while (dest[j])
@@ -39,14 +53,15 @@ int	main(int ac, char **av)
 	char *dest = av[1];
 	char *dest2 = av[1];
 	char *src = av[2];
+	char *src2 = av[2];
 	printf("my function: \n");
 	unsigned int r1 = ft_strlcat(dest, src, size);
 	printf("\tsource: %s\n", src);
 	printf("\tdest: %s\n", dest);
 	printf("\treturn: %d\n", r1);
-	unsigned int r2 = strlcat(dest2, src, size);
+	unsigned int r2 = strlcat(dest2, src2, size);
 	printf("string.h function: \n");
-	printf("\tsource: %s\n", src);
+	printf("\tsource: %s\n", src2);
 	printf("\tdest: %s\n", dest2);
 	printf("\treturn: %d\n", r2);
 	return (0);
