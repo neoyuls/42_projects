8189f44bbda5718aeb3d6fe16c47b0fb8462f465
neoyuls <vernon.juli@gmail.com>
2026-07-27 01:32:03 -0600

    finished (?) strncmp

--- libft_h/libft.a/src/ft_strncmp.c
+++ libft_h/libft.a/src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 00:49:43 by neoyuls           #+#    #+#             */
-/*   Updated: 2026/07/27 00:57:15 by neoyuls          ###   ########.fr       */
+/*   Updated: 2026/07/27 01:31:43 by neoyuls          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,26 @@ int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 	i = 0;
 	while (((s1[i] || s2[i]) != '\0') && i < n)
 	{
-
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
+	return (0);
 }
 // Ill do memcmp first then this one
+/*
+#include <string.h>
+#include <stdio.h>
+
+int	main(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	printf("my function: %d\n", ft_strncmp(av[1], av[2], strlen(av[1])));
+	printf("string.h function: %d\n", strncmp(av[1], av[2], strlen(av[1])));
+	return 0;
+}
+*/
