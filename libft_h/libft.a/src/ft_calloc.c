1235a4932a8ae318a7c5c5eca2e5d120a5c1af3b
neoyuls <vernon.juli@gmail.com>
2026-07-27 12:36:13 -0600

    started (and finished?) calloc

--- libft_h/libft.a/src/ft_calloc.c
+++ libft_h/libft.a/src/ft_calloc.c
@@ -6,8 +6,25 @@
 /*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/27 04:17:55 by neoyuls           #+#    #+#             */
-/*   Updated: 2026/07/27 04:17:56 by neoyuls          ###   ########.fr       */
+/*   Updated: 2026/07/27 12:35:28 by neoyuls          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
+void	*ft_calloc(unsigned int n, unsigned int size)
+{
+	unsigned char	*ptr;
+	unsigned int	i;
+
+	i = 0;
+	ptr = malloc(size);
+	while (i < n)
+	{
+		ptr[i] = 0;
+		i++;
+	}
+	if (n == 0)
+		*ptr = 0;
+	return (ptr);
+}
