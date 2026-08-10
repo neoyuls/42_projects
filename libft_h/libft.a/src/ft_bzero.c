22aae18a54ad522e75edb0296697c018190e4815
neoyuls <vernon.juli@gmail.com>
2026-08-10 13:23:31 -0600

    fixed bzero

--- libft_h/libft.a/src/ft_bzero.c
+++ libft_h/libft.a/src/ft_bzero.c
@@ -6,17 +6,15 @@
 /*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/20 17:42:34 by neoyuls           #+#    #+#             */
-/*   Updated: 2026/08/01 12:57:31 by neoyuls          ###   ########.fr       */
+/*   Updated: 2026/08/10 13:23:12 by neoyuls          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 // UNCLEAR IF DONE, REVISIT LATER, ALSO WRITE A MAIN LAZY ASS
 void	ft_bzero(void *s, unsigned int n)
 {
-	unsigned char	*ptr;
-
-	ptr = (unsigned char *)s;
 	if (n == 0)
 		return ;
-	ptr = ft_memset(s, 0, n);
+	ft_memset(s, 0, n);
 }
