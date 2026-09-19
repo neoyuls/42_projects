ad3fcfb329777b254d5e8f4dc95d7c3026a36788
neoyuls <vernon.juli@gmail.com>
2026-09-19 09:41:56 +0200

    finished strtrim

--- libft_h/libft_a/srcs/ft_strtrim.c
+++ libft_h/libft_a/srcs/ft_strtrim.c
@@ -6,61 +6,41 @@
 /*   By: neoyuls <neoyuls@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/15 04:55:07 by neoyuls           #+#    #+#             */
-/*   Updated: 2026/09/15 07:57:01 by neoyuls          ###   ########.fr       */
+/*   Updated: 2026/09/19 09:40:02 by jvernon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*allocate(char const *s1, char const *set)
+static int	check_chars(char const c, char const *set)
 {
-	char			*s;
-	unsigned int	size;
 	unsigned int	i;
-	unsigned int	j;
 
 	i = 0;
-	size = ft_strlen(s1);
-	while (s1[i])
+	while (set[i])
 	{
-		j = 0;
-		while (set[j])
-		{
-			if (s1[i] == set[j])
-			{
-				size--;
-			}
-			j++;
-		}
+		if (c == set[i])
+			return (1);
 		i++;
 	}
-	s = malloc(sizeof(char) * (size + 1));
-	return (s);
+	return (0);
 }
 
-char	*fill(char const *s1, char const *set, char *trimmed)
+static char	*fill(char const *s1, char const *set)
 {
+	unsigned int	i;
 	unsigned int	j;
-	unsigned int	k;
+	char			*trimmed;
 
-	k = 0;
-	while (*s1)
-	{
-		j = 0;
-		while (set[j])
-		{
-			if (set[j] == *s1)
-				break ;
-			j++;
-		}
-		if (set[j] == '\0')
-		{
-			trimmed[k] = *s1;
-			k++;
-		}
-		s1++;
-	}
-	trimmed[k] = '\0';
+	i = 0;
+	j = ft_strlen(s1);
+	if (j > 0)
+		j--;
+	while (check_chars(s1[i], set) == 1)
+		i++;
+	while (check_chars(s1[j], set) == 1 && j != 0)
+		j--;
+	trimmed = ft_substr(s1, i, j - i + 1);
 	return (trimmed);
 }
 
@@ -68,43 +48,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char			*trimmed;
 
-	trimmed = allocate(s1, set);
+	trimmed = fill(s1, set);
 	if (!trimmed)
 		return (NULL);
-	trimmed = fill(s1, set, trimmed);
 	return (trimmed);
 }
-
-/*
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char			*trimmed;
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	k;
-
-	i = 0;
-	k = 0;
-	trimmed = allocate(s1, set);
-	if (!trimmed)
-		return (NULL);
-	while (s1[i])
-	{
-		j = 0;
-		while (set[j])
-		{
-			if (set[j] == s1[i])
-				break ;
-			j++;
-		}
-		if (set[j] == '\0')
-		{
-			trimmed[k] = s1[i];
-			k++;
-		}
-		i++;
-	}
-	trimmed[k] = '\0';
-	return (trimmed);
-}
-*/
