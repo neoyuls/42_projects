5c3ed1099d2facf661d7530a4ab817b53f8346ed
neoyuls <vernon.juli@gmail.com>
2026-09-25 10:27:16 +0200

    made Projects/, to prevent the main directory from being too cluttered

--- projects/libft_h/libft_a/srcs/ft_lstclear.c
+++ projects/libft_h/libft_a/srcs/ft_lstclear.c
@@ -1,24 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstiter.c                                       :+:      :+:    :+:   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvernon <jvernon@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/23 06:53:06 by jvernon           #+#    #+#             */
-/*   Updated: 2026/09/23 07:28:41 by jvernon          ###   ########.fr       */
+/*   Created: 2026/09/23 04:12:44 by jvernon           #+#    #+#             */
+/*   Updated: 2026/09/25 02:55:25 by jvernon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f)(void *))
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	if (!f || !lst)
+	t_list	*node;
+	t_list	*nxt;
+
+	if (!lst || !(*lst) || !del)
 		return ;
-	while(lst)
+	nxt = *lst;
+	while (nxt)
 	{
-		f(lst->content);
-		lst = lst->next;
+		node = nxt->next;
+		ft_lstdelone(nxt, del);
+		nxt = node;
 	}
+	*lst = NULL;
 }
--- projects/libft_h/libft_a/srcs/ft_lstiter.c
+++ projects/libft_h/libft_a/srcs/ft_lstiter.c
@@ -1,30 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvernon <jvernon@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/23 04:12:44 by jvernon           #+#    #+#             */
-/*   Updated: 2026/09/23 06:49:03 by jvernon          ###   ########.fr       */
+/*   Created: 2026/09/23 06:53:06 by jvernon           #+#    #+#             */
+/*   Updated: 2026/09/25 02:55:41 by jvernon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstclear(t_list **lst, void (*del)(void *))
+void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	t_list	*node;
-	t_list	*nxt;
-
-	if (!lst || !(*lst) || !del)
+	if (!f || !lst)
 		return ;
-	nxt = *lst;
-	while (nxt)
+	while (lst)
 	{
-		node = nxt->next;
-		ft_lstdelone(nxt,del);
-		nxt = node;
+		f(lst->content);
+		lst = lst->next;
 	}
-	*lst = NULL;
 }
