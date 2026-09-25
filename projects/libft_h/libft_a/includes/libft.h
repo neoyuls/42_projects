cc3cc2b620d68aa50e667714588ae0f02c612e09
neoyuls <vernon.juli@gmail.com>
2026-09-25 11:23:00 +0200

    added lstiter to header and makefile

--- projects/libft_h/libft_a/includes/libft.h
+++ projects/libft_h/libft_a/includes/libft.h
@@ -64,5 +64,6 @@ t_list	*ft_lstlast(t_list *lst);
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 int		ft_lstsize(t_list *lst);
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
 
 #endif
